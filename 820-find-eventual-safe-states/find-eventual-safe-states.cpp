70b313ce5133a79be37120994d1d64b315c0ec06
Anurag019-hub <anurag160507@gmail.com>
2026-07-15 17:24:26 +0530

    Time: 33 ms (60.96%) | Memory: 66.4 MB (10.55%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,37 +1,56 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<bool> visited(n,false);
-        vector<vector<int>> copygraph(n);
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            for(auto ele: graph[i]){
-                    copygraph[ele].push_back(i);
-            }
-        }
 
-        vector<int> indegree(n);
-        for(int i=0;i<n;i++){
-            indegree[i] = graph[i].size();
-            if(indegree[i]==0){
-                q.push(i);
-                visited[i] = true;
-            }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            int curr = q.front();
-            ans.push_back(q.front());
-            q.pop();
-            for(auto ele : copygraph[curr]){
-                indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.push(ele);
-                }
-            }
-        }
-        if(ans.size()>0)sort(ans.begin(),ans.end());
-        return ans;
+
+//observations:
+	// if all the nodes from node 1 go to the terminal node then th node is safe…
+
+
+//intitution:
+//so question is when not possible to connect it with terminal node… 
+//so we can say that if cycle is present then only we can not do that…
+
+//approach-01:
+//we can make adjlist and indegree list for each ele
+// check if there is cycle (we can do it by toposort bfs/dfs) if cycle is present ignore nodes present in cycle;
+
+//Implementation:
+int n = graph.size();
+vector<vector<int>> adjList(n);//adjList will store the edges directed from another  node to current node 
+vector<int> inDegree(n);//store the number of nodes are coming to that node
+//traverse the graph and get all adjacent nodes of node…
+for (int i = 0; i < n; i++) {
+    for (int neighbor : graph[i]) { // "For every neighbor in graph[i]"
+        adjList[neighbor].push_back(i);
+        inDegree[i]++;
+    }
+}
+
+//make a queue and put all the which are terminal (inDegree[i] == 0)
+queue<int> bfs_queue;
+for(int i=0;i<n;i++){
+	if(inDegree[i]==0)
+		 bfs_queue.push(i);
+}
+
+// after that we can check for cycle through topo sort if we take out node and remove the connection from its adjNode and if the indgeree become zero that mean there is no cycle…
+vector<int> safeNode;
+while(!bfs_queue.empty()){
+	int curr_node = bfs_queue.front();
+	safeNode.push_back(bfs_queue.front());
+	bfs_queue.pop();
+	for(int node: adjList[curr_node]){
+		inDegree[node]--;
+		if(inDegree[node]==0) // because there is no cycle and it can reach easily to terminal node..
+			bfs_queue.push(node);
+	}
+}
+sort(safeNode.begin(),safeNode.end());
+return safeNode;
+
+//TC - O(N^2);
+//SC - O(3*N ~= N);
     }
 };
+
